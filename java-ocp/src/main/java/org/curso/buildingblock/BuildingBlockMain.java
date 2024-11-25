127d6967725e4d43b560c2788a39a771c43912e1
Camilo Rivera Ladino <backenddeveloper30@gmail.com>
2024-11-25 12:49:50 -0500

    Commit inicial

---
+++ java-ocp/src/main/java/org/curso/buildingblock/BuildingBlockMain.java
@@ -0,0 +1,9 @@
+package org.curso.buildingblock;
+
+public class BuildingBlockMain {
+
+    public static void main(String[] args) {
+        double value = 102.0;
+        System.out.println(value);
+    }
+}
